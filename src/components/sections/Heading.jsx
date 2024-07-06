a18a1337527b1ba0f423f50439fc111031bff40b
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-07 03:13:22 +0800

    Update Heading.jsx

--- src/components/sections/Heading.jsx
+++ src/components/sections/Heading.jsx
@@ -5,7 +5,7 @@ const Heading = () => {
   return (
     <div className='flex flex-col w-full items-center justify-center py-20 gap-5'>
             <h2 className='font-semibold sm:text-[40px] text-[30px]'>Notes</h2>
-            <p className='mb-3 text-justify max-w-[600px] h-auto leading-7 text-third sm:text-[16px] text-[14px]'>James here, I'm still working on developing the website, iNTFibeR. I've used the same materials and labels on every division. 
+            <p className='mb-3 text-justify max-w-[600px] h-auto leading-7 text-third sm:text-[16px] text-[14px] sm:px-0 px-3'>James here, I'm still working on developing the website, iNTFibeR. I've used the same materials and labels on every division. 
               However, I'll start working on that, as well as the server and database once I've finished developing the entire front-end of the website. 
               The website will be developed using NodeJS for server management, FireBase for database management, ViteJS & Tailwind CSS for front-end.</p>
             <Button link="#" label="Button"></Button>
