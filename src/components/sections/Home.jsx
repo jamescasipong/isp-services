00dd46ef4251498d4f0a97f6d7666257dc0054aa
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-06 19:19:31 +0800

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <section id="home" className='flex w-full justify-center items-center flex-col gap-y-20'>
         <div className='flex-col flex flex-1 items-center justify-center w-full py-10 mt-40 '>
             <h1 className='font-bold text-6xl mb-5'>Welcome to iNTFibeR</h1>
-            <p className='mb-5 description max-w-[600px] text-center'>iNTFiber is an ISP service provider in the Philippines that provides the fastest internet in the country.
+            <p className='mb-5 description max-w-[600px] text-center'>iNTFiber is an ISP service provider in the Philippines that provides one of the fastest internet in the country.
             </p>
             <Button link="#" label="Get Started!"></Button>
         </div>
