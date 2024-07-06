415b545205ff1a706420b94c5be4b67ea9349c93
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-07 03:06:35 +0800

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({link, label}) => {
   return (
     
-    <button href={link} className='button text-white'>{label}</button>
+    <button href={link} className='button text-white sm:text-[16px] text-[14px]'>{label}</button>
   )
 }
 
