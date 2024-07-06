0e7b8d9c5b367e582b70469dda02cf5fbc8dcc2d
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-06 21:57:27 +0800

    add div that will disable the entire home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,31 @@ function App() {
 
   return (
     <div className="bg-primary w-full overflow-hidden">
+
+      
       <div className="flex justify-center items-center md:px-4 px-4">
         <div className="xl:max-w-[1280px] w-full">
             <Navbar></Navbar>
         </div>
       </div>
-
-      <div className="flex justify-center items-start">
-        <div className="xl:max-w-[1280px] w-full  md:px-4 px-8">
-            <Home></Home>
+      
+      <div className='block'>
+        <div className="flex justify-center items-start">
+          <div className="xl:max-w-[1280px] w-full  md:px-4 px-8">
+              <Home></Home>
+          </div>
         </div>
-      </div>
 
-      <div className="flex justify-center items-start bg-bg mt-20">
-        <div className="xl:max-w-[1280px] w-full  md:px-4 px-4">
-            <Heading></Heading>
+        <div className="flex justify-center items-start bg-bg mt-20">
+          <div className="xl:max-w-[1280px] w-full  md:px-4 px-4">
+              <Heading></Heading>
+          </div>
         </div>
-      </div>
 
-      <div className="flex justify-center items-star">
-        <div className="xl:max-w-[1280px] w-full  md:px-4 px-4">
-            
+        <div className="flex justify-center items-star">
+          <div className="xl:max-w-[1280px] w-full  md:px-4 px-4">
+              
+          </div>
         </div>
       </div>
     </div>
