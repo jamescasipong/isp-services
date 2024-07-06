14f637c3038bd8472fe75cdf76351cf08a72d88a
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-06 22:21:17 +0800

    Update Heading.jsx

--- src/components/sections/Heading.jsx
+++ src/components/sections/Heading.jsx
@@ -5,7 +5,7 @@ const Heading = () => {
   return (
     <div className='flex flex-col w-full items-center justify-center py-20 gap-5'>
             <h2 className='font-semibold text-[40px]'>Heading</h2>
-            <p className='mb-3 sm:text-start text-center'>James here, I'm still working on the development of iNTFibeR.</p>
+            <p className='mb-3 text-center max-w-[600px] h-auto'>James here, I'm still working on developing the website, iNTFibeR. I've used random materials and the same labels on every division.</p>
             <Button link="#" label="Button"></Button>
     </div>
   )
