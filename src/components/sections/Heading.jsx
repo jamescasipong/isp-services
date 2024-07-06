77e2b8adfae9d6f0d61b09aebc34999b63b7506f
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-06 22:12:25 +0800

    sd

--- src/components/sections/Heading.jsx
+++ src/components/sections/Heading.jsx
@@ -5,7 +5,7 @@ const Heading = () => {
   return (
     <div className='flex flex-col w-full items-center justify-center py-20 gap-5'>
             <h2 className='font-semibold text-[40px]'>Heading</h2>
-            <p className='mb-4'>Plus a subheading for your site’s footer</p>
+            <p className='mb-3'>As you can see, the website is still under development</p>
             <Button link="#" label="Button"></Button>
     </div>
   )
