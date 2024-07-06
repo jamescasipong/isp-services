b402e402cb99da7bac5a932a4bf177f185e69d4d
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-06 22:18:09 +0800

    Update Heading.jsx

--- src/components/sections/Heading.jsx
+++ src/components/sections/Heading.jsx
@@ -5,7 +5,7 @@ const Heading = () => {
   return (
     <div className='flex flex-col w-full items-center justify-center py-20 gap-5'>
             <h2 className='font-semibold text-[40px]'>Heading</h2>
-            <p className='mb-3'>As you can see, the website is still under development</p>
+            <p className='mb-3 sm:text-start text-center'>James here, I'm still working on the development of iNTFibeR.</p>
             <Button link="#" label="Button"></Button>
     </div>
   )
