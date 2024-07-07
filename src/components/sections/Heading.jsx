a14d240f802896e75594e2b39a5db9b79648a1b0
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-08 00:48:14 +0800

    Update Heading.jsx

--- src/components/sections/Heading.jsx
+++ src/components/sections/Heading.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Button from '../Button.jsx';
 
+const handleClick = () => {
+  window.open('https://github.com/jamescasipong/isp-services', '_blank');
+};
+
 const Heading = () => {
   return (
     <div className='flex flex-col w-full items-center justify-center py-20 gap-5'>
@@ -8,7 +12,7 @@ const Heading = () => {
             <p className='mb-3 text-justify max-w-[600px] h-auto leading-7 text-third sm:text-[16px] text-[14px] sm:px-0 px-3'>James here, I'm still working on developing the website, iNTFibeR. I've used the same materials and labels on every division. 
               However, I'll start working on that, as well as the server and database once I've finished developing the entire front-end of the website. 
               This will be developed using NodeJS for server management, FireBase for database management, ViteJS & Tailwind CSS for front-end. If you're wondering what iNTFibeR means, it's just some random title I've come up with. lmao.</p>
-            <Button link="location.href='https://google.com';" label="Check out on GitHub"></Button>
+            <Button link={handleClick} label="Check out on GitHub"></Button>
     </div>
   )
 }
