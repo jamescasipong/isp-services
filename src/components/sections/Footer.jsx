26be7edc71d7a1f9dc412869f5fdf6e6383d7faf
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-08 18:01:15 +0800

    Update Footer.jsx

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className='flex w-full sm:flex-row flex-col justify-center items-start sm:py-16 py-10 sm:px-10 px-2'>
         <div className='flex-1 flex flex-col gap-20'>
-            <h3>Site Name</h3>
+            <h3 className='font-medium'>Site Name</h3>
 
             <div className='flex-1 flex flex-row gap-5 items-center'>
                 <a href="#"><img src={facebook} alt="facebook" className='object-contain'/></a>
@@ -19,7 +19,7 @@ const Footer = () => {
         <div className='flex-1 flex-wrap flex justify-between w-full md:mr-36 m-0 sm:mt-0 mt-3'>
             {footer.map((f) => (
                 <div key={f.key} className='flex flex-col'>
-                   <a href={`#${f.id}`}><h4 className='sm:mt-0 mt-5 font-medium hover:text-orange-500' >{f.title}</h4></a>
+                   <a href={`#${f.id}`}><h4 className='sm:mt-0 mt-5 font-medium hover:text-orange-500   ' >{f.title}</h4></a>
 
                    <ul className='flex flex-col gap-2 mt-2'>
                       {f.links.map((link) => (
