db607b64b5ab9d06ad7c5bbdce0eb85d886b0621
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-10 19:56:35 +0800

    Update Header.jsx

--- src/components/ContactSections/Header.jsx
+++ src/components/ContactSections/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     
   return (
     <div className='w-full justify-center items-center flex flex-1 flex-col'>
-        <h1 className='font-bold sm:text-6xl text-[40px] mb-5 sm:text-start text-center sm:mt-40 mt-10'>Pricing page title</h1>
+        <h1 className='font-bold sm:text-6xl text-[40px] mb-5 sm:text-start text-center sm:mt-40 mt-10'>OptiNet's features</h1>
 
         <p className='mb-5 sm:max-w-[650px] sm:text-center text-justify sm:text-[24px] text-[18px] text-third'>
             And a subheading describing your pricing plans, too
@@ -17,16 +17,16 @@ const Header = () => {
 
             <Switch></Switch>
 
-            <div className='w-full flex sm:flex-row flex-col gap-10 justify-center items-center my-10 md:p-10 p-0'>
+            <div className='w-full flex ss:flex-row flex-col sm:gap-10 gap-3 justify-center items-center my-10 md:p-10 p-0'>
                 {plans.map((p) => (
                     <div className='shadow-2xl md:p-10 p-7 rounded-md flex flex-col w-full' key={p.id}>
                         <p>{p.name}</p>
-                        <div className='flex items-center justify-start relative'>
+                        <div className='flex items-center justify-start'>
                             <h1 className='font-semibold text-[40px]'>{p.prices}</h1>
-                            <p className='p-[16px] font-medium top-0'>{p.deadline}</p>
+                            <p className='px-[5px] mt-2 font-medium top-0'>{p.deadline}</p>
                         </div>
                         
-                        <ul className='list-disc px-10 my-4'>
+                        <ul className='list-disc px-9 my-4'>
                             <li>{p.f1}</li>
                             <li>{p.f2}</li>
                             <li>{p.f3}</li>
