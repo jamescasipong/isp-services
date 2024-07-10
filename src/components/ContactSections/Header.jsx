37f1f2ce7e5dde79c4d062a6af9b7815170efce0
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-10 18:16:26 +0800

    Update Header.jsx

--- src/components/ContactSections/Header.jsx
+++ src/components/ContactSections/Header.jsx
@@ -7,9 +7,11 @@ const Header = () => {
   return (
     <div className='w-full justify-center items-center flex flex-1 flex-col'>
         <h1 className='font-bold sm:text-6xl text-[40px] mb-5 sm:text-start text-center sm:mt-40 mt-10'>Pricing page title</h1>
-            <p className='mb-5 sm:max-w-[650px] sm:text-center text-justify sm:text-[24px] text-[18px] text-third'>
+
+        <p className='mb-5 sm:max-w-[650px] sm:text-center text-justify sm:text-[24px] text-[18px] text-third'>
             And a subheading describing your pricing plans, too
-            </p>
+        </p>
+        
     </div>
   )
 }
