ce83d6dd46e6ebe0c5bcc9c28621c71926286721
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-20 02:56:36 +0800

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,48 +1,50 @@
-import {avatar, globe, calendar, lock} from "../assets/index.js";
+import { avatar, globe, calendar, lock } from "../assets/index.js";
 
 export const navLinks = [
-    {
-      id: "/",
-      title: "Home",
-    },
-    {
-      id: "features",
-      title: "Features",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
+  {
+    id: "/",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
 
 export const heading = [
-    {
-        id: "1",
-        logo: avatar,
-        title: "Subheading",
-        description: "Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story."
-    },
-    {
-        id: "2",
-        logo: globe,
-        title: "Subheading",
-        description: "Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story."
-    },
-    {
-        id: "3",
-        logo: calendar,
-        title: "Subheading",
-        description: "Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story."
-    },
-    {
-        id: "4",
-        logo: lock,
-        title: "Subheading",
-        description: "Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story."
-    },
-    
-]
-
+  {
+    id: "1",
+    logo: avatar,
+    title: "Subheading",
+    description:
+      "Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.",
+  },
+  {
+    id: "2",
+    logo: globe,
+    title: "Subheading",
+    description:
+      "Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.",
+  },
+  {
+    id: "3",
+    logo: calendar,
+    title: "Subheading",
+    description:
+      "Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.",
+  },
+  {
+    id: "4",
+    logo: lock,
+    title: "Subheading",
+    description:
+      "Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.",
+  },
+];
 
 export const footer = [
   {
@@ -65,7 +67,7 @@ export const footer = [
         name: "Page",
         links: "#",
       },
-    ]
+    ],
   },
   {
     key: "2",
@@ -87,7 +89,7 @@ export const footer = [
         name: "Page",
         links: "#",
       },
-    ]
+    ],
   },
   {
     key: "3",
@@ -109,41 +111,40 @@ export const footer = [
         name: "Page",
         links: "#",
       },
-    ]
+    ],
   },
 ];
 
-export const plans =
-[
+export const plans = [
   {
     id: "1",
-    name: "Plan 1",
-    prices: "$5",
+    name: "FiberX",
+    prices: "₱1500",
     deadline: "per month",
-    f1: "Feature",
-      f2: "Feature",
-      f3: "Feature",
-      f4: "Feature",
+    f1: " Stream Music,  download movies and photos",
+    f2: " Video conferencing",
+    f3: "Free Access to BlastTV",
+    f4: "Data Roaming",
   },
   {
     id: "2",
-    name: "Plan 2",
-    prices: "$5",
+    name: "FiberZ",
+    prices: "₱2500",
     deadline: "per month",
     f1: "Feature",
-      f2: "Feature",
-      f3: "Feature",
-      f4: "Feature",
+    f2: "Feature",
+    f3: "Feature",
+    f4: "Feature",
   },
 
   {
     id: "3",
-    name: "Plan 3",
-    prices: "$5",
+    name: "FiberPlus",
+    prices: "$3500",
     deadline: "per month",
     f1: "Feature",
-      f2: "Feature",
-      f3: "Feature",
-      f4: "Feature",
+    f2: "Feature",
+    f3: "Feature",
+    f4: "Feature",
   },
 ];
