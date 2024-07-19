3f9c4467014d2a144916f60616195e6642c7fd29
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-20 00:00:27 +0800

    Update Signup.jsx

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Signup = () => {
-  return <div></div>;
+  return (
+    <div className="h-screen max-w-[100%] items-center flex justify-center">
+      <div className="flex flex-col items-center">
+        <h3>Create an account</h3>
+        <p>Enter your email to sign up for this app</p>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
