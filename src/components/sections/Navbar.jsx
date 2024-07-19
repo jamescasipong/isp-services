667f025edb5226dc1924dccb39dbb5549601163c
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-19 23:48:43 +0800

    add prompt to buttons

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import {navLinks} from "../../constants";
-
-
-
+import React from "react";
+import { navLinks } from "../../constants";
 
 const Navbar = () => {
   return (
+    <section
+      id="navbar"
+      className=" flex justify-between w-full py-5 items-center"
+    >
+      <div>
+        <h2 className="text-black font-medium text-[24px] sm:px-0">OptiNet</h2>
+      </div>
 
-    <section id='navbar' className=' flex justify-between w-full py-5 items-center'> 
-        <div>
-            <h2 className='text-black font-medium text-[24px] sm:px-0'>OptiNet</h2>
-        </div>
+      <div className="flex-row gap-10 sm:flex hidden">
+        {navLinks.map((nL) => (
+          <ul key={nL.id} className="flex items-center">
+            <li
+              className={`text-black cursor-pointer font-medium navtext hover:text-orange-500 ${nL.id}`}
+            >
+              <a href={`${nL.id} `}>{nL.title}</a>
+            </li>
+          </ul>
+        ))}
 
-        <div className='flex-row gap-10 sm:flex hidden'>
-            {navLinks.map((nL) =>
-            (
-                <ul key={nL.id} className='flex items-center'>
-                    <li className={`text-black cursor-pointer font-medium navtext hover:text-orange-500 ${nL.id}`}><a href={`${nL.id} `}>{nL.title}</a></li>
-                </ul>
-                
-            ))}
-
-            <button className='button text-white text-[18px]'>Sign Up</button>
-
-            
-        </div>
-        
+        <a href="/signup" className="button text-white text-[18px]">
+          Sign Up
+        </a>
+      </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
