7579a3821c69f4c7f96ed9b7271a6047d45adc8a
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-20 02:21:54 +0800

    add nav for mobile

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { navLinks } from "../../constants";
 import React, { useState } from "react";
 
 const Navbar = () => {
-  const [isToggle, setToggle] = !isToggle;
+  const [isToggle, setToggle] = useState(false);
 
   return (
     <section
@@ -28,6 +27,35 @@ const Navbar = () => {
         <a href="/signup" className="button text-white text-[18px]">
           Sign Up
         </a>
+      </div>
+
+      <div className="sm:hidden">
+        <ul>
+          <i
+            className={`${
+              !isToggle ? "fa-solid fa-bars" : "fa-solid fa-x"
+            } cursor-pointer`}
+            onClick={() => setToggle(!isToggle)}
+          ></i>
+        </ul>
+      </div>
+
+      <div
+        className={`absolute z-[3px] left-0 right-0 bg-white ${
+          !isToggle ? "top-[-600px]" : "top-[70px]"
+        }  flex-col items-center justify-center rounded-md border-[1px] border-black transition-all duration-300 sm:top-[-600px] top-[70px]`}
+      >
+        {navLinks.map((nL) => (
+          <ul key={nL.id} className="flex items-center justify-center">
+            <a className="text-center w-full" href={`${nL.id} `}>
+              <li
+                className={`text-black cursor-pointer font-medium navtext hover:text-orange-500 hover:bg-slate-300 ${nL.id} py-5 w-full`}
+              >
+                {nL.title}
+              </li>
+            </a>
+          </ul>
+        ))}
       </div>
     </section>
   );
