d0d2b152c972cca6d2d6f777abcc4c17687b0761
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-21 04:43:59 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const UserAccount = require("./models/Users");
+require("dotenv").config();
 
 const app = express();
 
@@ -9,28 +10,34 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://jamesxcasipong:!Unravel12345@cluster0.yqpkrko.mongodb.net/NODE-APIs?retryWrites=true&w=majority&appName=Cluster0"
-);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB:", err.message));
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  huhu: String,
+});
+
+const admin = mongoose.model("admin", UserSchema);
 
 app.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Find user by email
     const user = await UserAccount.findOne({ email });
 
     if (!user) {
-      // If user is not found
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Check if the password is correct
-    if (user.password == password) {
-      // Password is correct
+    if (user.password === password) {
       res.json("success");
     } else {
-      // Password is incorrect
       res.status(401).json({ message: "Password incorrect" });
     }
   } catch (error) {
@@ -39,19 +46,16 @@ app.post("/signin", async (req, res) => {
   }
 });
 
-// Example route to get users
 app.post("/signup", async (req, res) => {
   const { email } = req.body;
 
   try {
-    // Check if the user already exists
     const existingUser = await UserAccount.findOne({ email });
 
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // If user doesn't exist, create a new user account
     const newUser = await UserAccount.create(req.body);
     res.status(200).json(newUser);
   } catch (error) {
@@ -63,5 +67,5 @@ app.post("/signup", async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
