7b8b3855a38aeae83f04f00964ec411b26c86995
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-20 22:11:53 +0800

    add hyperlink to join now

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({link, label}) => {
+const Button = ({ link, label }) => {
   return (
-    
-    <button onClick={link} className='button text-white sm:text-[16px] text-[14px]'>{label}</button>
-  )
-}
+    <button
+      onClick={() => {
+        window.location.href = link;
+      }}
+      className="button text-white sm:text-[16px] text-[14px]"
+    >
+      {label}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
