17ca6e02c11e8f542027a174d20ce598b09714c1
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-21 01:51:04 +0800

    xd

--- server/index.js
+++ server/index.js
@@ -1,67 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-const UserAccount = require("./models/Users");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
-
-app.use(express.json());
-app.use(cors());
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://jamesxcasipong:!Unravel12345@cluster0.yqpkrko.mongodb.net/NODE-APIs?retryWrites=true&w=majority&appName=Cluster0"
-);
+mongoose
+  .connect(
+    "mongodb+srv://jamesxcasipong:!Unravel12345@cluster0.yqpkrko.mongodb.net/NODE-APIs?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-app.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "../public")));
 
-  try {
-    // Find user by email
-    const user = await UserAccount.findOne({ email });
-
-    if (!user) {
-      // If user is not found
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // Check if the password is correct
-    if (user.password == password) {
-      // Password is correct
-      res.json("success");
-    } else {
-      // Password is incorrect
-      res.status(401).json({ message: "Password incorrect" });
-    }
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: "Server error" });
-  }
+// Define a simple route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-// Example route to get users
-app.post("/signup", async (req, res) => {
-  const { email } = req.body;
-
-  try {
-    // Check if the user already exists
-    const existingUser = await UserAccount.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    // If user doesn't exist, create a new user account
-    const newUser = await UserAccount.create(req.body);
-    res.status(200).json(newUser);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
+// Catch-all route to handle other paths
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-// Start the server
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`it works`);
+  console.log(`Server is running on port ${PORT}`);
 });
