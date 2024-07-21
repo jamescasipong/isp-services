06f76745155c86ea4e61ed50b09d0dc9e8926134
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-22 00:17:18 +0800

    xd

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 const UserAccount = require("./models/Users");
 require("dotenv").config();
 const app = express();
@@ -9,20 +10,26 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://jamesxcasipong:!Unravel12345@cluster0.yqpkrko.mongodb.net/NODE-APIs?retryWrites=true&w=majority&appName=Cluster0"
-);
+mongoose
+  .connect(process.env.CONST_USERS_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
+// GET all users (for debugging or admin purposes)
 app.get("/", async (req, res) => {
-  UserAccount.find({})
-    .then(function (users) {
-      res.json(users);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+  try {
+    const users = await UserAccount.find({});
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
+// Signin route
 app.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
@@ -31,16 +38,15 @@ app.post("/signin", async (req, res) => {
     const user = await UserAccount.findOne({ email });
 
     if (!user) {
-      // If user is not found
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Check if the password is correct
-    if (user.password == password) {
-      // Password is correct
+    // Compare hashed passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
       res.json("success");
     } else {
-      // Password is incorrect
       res.status(401).json({ message: "Password incorrect" });
     }
   } catch (error) {
@@ -49,9 +55,9 @@ app.post("/signin", async (req, res) => {
   }
 });
 
-// Example route to get users
+// Signup route
 app.post("/signup", async (req, res) => {
-  const { email } = req.body;
+  const { email, password, firstName, lastName } = req.body;
 
   try {
     // Check if the user already exists
@@ -61,17 +67,25 @@ app.post("/signup", async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // If user doesn't exist, create a new user account
-    const newUser = await UserAccount.create(req.body);
+    // Hash the password before saving it to the database
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user account
+    const newUser = await UserAccount.create({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+
     res.status(200).json(newUser);
   } catch (error) {
-    console.log(error.message);
+    console.error(error.message);
     res.status(500).json({ message: error.message });
   }
 });
 
 // Start the server
-
 app.listen(3001, () => {
   console.log(`Server is running on port http://localhost:3001`);
 });
