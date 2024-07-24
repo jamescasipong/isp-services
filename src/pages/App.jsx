84a268b9ad08cbdffe763ee307c289c380e5800d
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-25 04:23:47 +0800

    xd

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -14,8 +14,8 @@ import AdminHome from "./Admin/AdminHome";
 import AdminSignIn from "./Admin/AdminSignIn";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/";
-//axios.defaults.baseURL = "https://optinet-api-dev.vercel.app/";
+//axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://optinet-api-dev.vercel.app/";
 axios.defaults.withCredentials = true;
 
 function App() {
