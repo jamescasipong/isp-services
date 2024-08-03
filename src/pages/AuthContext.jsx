d66385e497afe9708ceb27f7908f5e3fdd30a54c
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-08-04 02:39:10 +0800

    Update AuthContext.jsx

--- src/pages/AuthContext.jsx
+++ src/pages/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import LoadingUI from "../components/LoadingUI"; // Assuming this is a loading spinner or similar component
+import LoadingUI from "../components/LoadingUI";
 
 export const AuthContext = createContext({});
 
