0a12a6dd92a656a9088c9af1e89f9c65b81b0aa8
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-08-04 02:40:40 +0800

    Update LoadingUI.jsx

--- src/components/LoadingUI.jsx
+++ src/components/LoadingUI.jsx
@@ -6,7 +6,7 @@ const LoadingUI = () => {
       <div role="status">
         <svg
           aria-hidden="true"
-          className="w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="w-20 h-20 text-gray-200 animate-spin dark:text-primary fill-[#8D2424]"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
